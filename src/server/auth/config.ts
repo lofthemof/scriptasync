4ba13442e065bdc0d75c5df40ed5a81918f17045
lofthemof <joshuajiananli@gmail.com>
2025-11-28 17:34:34 -0500

    adding pages to authConfig

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -43,6 +43,13 @@ export const authConfig = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    // verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   callbacks: {
     session: ({ session, token }) => ({
       ...session,
