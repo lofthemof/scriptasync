a48a4f8b902c676a6daa72b1042d27e18e28dd1a
lofthemof <joshuajiananli@gmail.com>
2025-12-05 18:24:44 -0500

    slight changes to user deletion router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,14 +1,17 @@
-import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  delete: protectedProcedure
-    .input(z.object({ userId: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.prisma.user.update({
-        where: { id: input.userId },
-        data: { name: null, email: null, image: null }, // TODO: add deletedAt on users, accounts, sessions, etc.
-      });
-      return { success: true };
-    }),
+  delete: protectedProcedure.mutation(async ({ ctx }) => {
+    const userId = ctx.session.user.id;
+
+    const user = await ctx.prisma.user.findUnique({ where: { id: userId } });
+    if (!user) throw new TRPCError({ code: "NOT_FOUND" });
+
+    await ctx.prisma.user.update({
+      where: { id: userId },
+      data: { name: null, email: null, image: null }, // TODO: add deletedAt on users, accounts, sessions, etc.
+    });
+    return { success: true };
+  }),
 });
